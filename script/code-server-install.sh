354e5bd3b8dabce8a35cc3cd3be6d29055095c7a
keke0123 <keke0123@naver.com>
2022-10-22 15:35:56 +0900

    Feat: extension 추가

--- script/code-server-install.sh
+++ script/code-server-install.sh
@@ -8,6 +8,26 @@ cat ~/.config/code-server/config.yaml
 # code server systemctl 등록 및 시작
 sudo systemctl enable --now code-server@$USER
 
+# install extension
+code-server --install-extension aaron-bond.better-comments
+code-server --install-extension naumovs.color-highlight
+code-server --install-extension usernamehw.errorlens
+code-server --install-extension mhutchie.git-graph
+code-server --install-extension github.vscode-pull-request-github
+code-server --install-extension eamodio.gitlens
+code-server --install-extension orta.vscode-jest
+code-server --install-extension firsttris.vscode-jest-runner
+code-server --install-extension arjun.swagger-viewer
+code-server --install-extension esbenp.prettier-vscode
+code-server --install-extension hashicorp.terraform
+code-server --install-extension vscode-icons-team.vscode-icons
+code-server --install-extension jpoissonnier.vscode-styled-components
+code-server --install-extension redhat.vscode-yaml
+code-server --install-extension golang.go
+code-server --install-extension ms-python.python
+
+sudo systemctl restart code-server@$USER
+
 # code server cli
 # 서비스 등록 (최초1회 실행)
 # sudo systemctl enable --now code-server@$USER
---
+++ script/lifecycle.sh
@@ -0,0 +1,1 @@
+# TODO. heartbeat check 후 종료시키는 로직
